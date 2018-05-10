1cab43d4dba00b14e0a55848c19a570600c72df8
Joseph Gilgen <gilgenlabs@gmail.com>
2018-05-10 11:27:37 -0700

    added new images (red, curly, bef_aft_1)

--- src/menu/index.jsx
+++ src/menu/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { StyleSheet, css } from 'aphrodite'
+import React from "react"
+import ReactDOM from "react-dom"
+import { StyleSheet, css } from "aphrodite"
 
-import { prices } from './prices'
-import Section from './Section'
-import Header from './header'
+import { prices } from "./prices"
+import Section from "./Section"
+import Header from "./header"
 
 const windowWidth = window.innerWidth
 const windowHeight = window.innerHeight
@@ -15,9 +15,9 @@ const sectionHeight = windowHeight * (7 / 9) // 840px on a 1080p resolution
 
 const styles = StyleSheet.create({
   menu: {
-    display: 'flex',
-    flexFlow: 'row wrap',
-    justifyContent: 'flex-start',
+    display: "flex",
+    flexFlow: "row wrap",
+    justifyContent: "flex-start",
     width: windowWidth,
     height: windowHeight,
   },
@@ -32,47 +32,42 @@ const styles = StyleSheet.create({
   promo1: {
     width: windowHeight / 3,
     height: windowHeight / 3,
-  }
+  },
 })
 
 export default class Menu extends React.Component {
-
   resize = () => this.forceUpdate()
 
   componentDidMount() {
-    window.addEventListener('resize', this.resize)
+    window.addEventListener("resize", this.resize)
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.resize)
+    window.removeEventListener("resize", this.resize)
   }
 
   render() {
     const promo1 = [
-      'fade1.jpg',
-      'cut1.jpg',
-      'fade3.jpg',
-      'fade2.jpg',
-      'withbeard.jpg',
-      'style1.jpg',
+      "fade1.jpg",
+      "cut1.jpg",
+      "fade3.jpg",
+      "fade2.jpg",
+      "withbeard.jpg",
+      "style1.jpg",
       //'600x600.png',
-    ].map((promo, i) =>
+    ].map((promo, i) => (
       <img
         key={i}
         className={`promo1-image ${css(styles.promo1)}`}
-        src={`../../assets/images/${promo}`}
+        src={`../assets/images/${promo}`}
         data-position={i}
       />
-    )
+    ))
     const promo2 = [
-      'before_after1.jpg',
-      'before_after2.jpg',
-      'before_after3.jpg',
-      'before_after4.jpg',
-      'before_after5.jpg',
-      'highlights1.jpg',
-      'highlights2.jpg',
-    ].map((promo, i) =>
+      "before_after_1.jpg",
+      "curly_ends.jpg",
+      "dark_red_highlights.jpg",
+    ].map((promo, i) => (
       <img
         key={i}
         className="promo2-image"
@@ -81,26 +76,26 @@ export default class Menu extends React.Component {
           height: windowHeight / 1.8,
         }}
         data-position={i}
-        src={`../../assets/images/${promo}`}
+        src={`../assets/images/${promo}`}
       />
-    )
+    ))
 
     return (
       <div className={css(styles.menu)} id="menu">
-        <Header className={css(styles.mainHeader)}/>
+        <Header className={css(styles.mainHeader)} />
         <div className={css(styles.section)}>
           <Section
-            name='haircuts'
-            description=''
-            items={prices['haircuts']}
+            name="haircuts"
+            description=""
+            items={prices["haircuts"]}
             promos={promo1}
           />
         </div>
         <div className={css(styles.section)}>
           <Section
-            name='style and beauty'
-            description=''
-            items={prices['style and beauty']}
+            name="style and beauty"
+            description=""
+            items={prices["style and beauty"]}
             promos={promo2}
           />
         </div>
@@ -109,7 +104,4 @@ export default class Menu extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <Menu/>,
-  document.getElementById('app')
-)
+ReactDOM.render(<Menu />, document.getElementById("app"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,49 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+
+const pathsToClean = ["dist"]
 
 var config = {
   resolve: {
-    extensions: ['.js','.jsx']
+    extensions: [".js", ".jsx"],
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: [ 'babel-loader' ],
+        loaders: ["babel-loader"],
         exclude: /node_modules/,
-        include: __dirname
+        include: __dirname,
       },
       {
         test: /\.css?$/,
-        loader: 'style-loader!css-loader',
-        include: __dirname
-      }
-    ]
+        loader: "style-loader!css-loader",
+        include: __dirname,
+      },
+    ],
   },
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    })
-  ]
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new CleanWebpackPlugin(pathsToClean),
+    new HtmlWebpackPlugin({
+      template: "src/menu/index.html",
+      inject: false,
+    }),
+  ],
 }
 
 var menuConfig = Object.assign({}, config, {
-  entry: './src/menu/index',
+  entry: "./src/menu/index",
   output: {
-    path: path.join(__dirname,'dist/menu'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, "dist/"),
+    filename: "bundle.[chunkHash].js",
   },
 })
 
