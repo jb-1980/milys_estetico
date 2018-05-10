c55977116aab24535418568da84332f7e4a9ddf1
Joseph Gilgen <gilgenlabs@gmail.com>
2018-05-10 11:34:30 -0700

    fix src for head image

--- src/menu/header.jsx
+++ src/menu/header.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import {StyleSheet, css} from 'aphrodite'
+import React from "react"
+import { StyleSheet, css } from "aphrodite"
 
-const background = '#dec8a8'
+const background = "#dec8a8"
 
 const styles = StyleSheet.create({
   header: {
-    display: 'flex',
-    justifyContent: 'space-around',
+    display: "flex",
+    justifyContent: "space-around",
     width: window.innerWidth,
     height: window.innerHeight * (1 / 5),
   },
   title: {
     flex: 1,
-    fontFamily: 'Sacramento',
-    fontSize: `${1/5 * 100}vh`,
-    letterSpacing: '0.3rem',
-    color: '#fefefe',
-    marginRight: '3rem',
-    marginLeft: '3rem',
+    fontFamily: "Sacramento",
+    fontSize: `${1 / 5 * 100}vh`,
+    letterSpacing: "0.3rem",
+    color: "#fefefe",
+    marginRight: "3rem",
+    marginLeft: "3rem",
     //textShadow: `-1px 0 7px #555, 1px 0 7px #555, 0 -1px 7px #555, 0 1px 7px #555`,
   },
   logoImage: {
@@ -26,14 +26,17 @@ const styles = StyleSheet.create({
   },
 })
 
-
-const Header = () =>
+const Header = () => (
   <div className={css(styles.header)}>
     <h1 style={{ marginBottom: 0, marginTop: 0 }}>
       <span className={css(styles.title)}>Mily's</span>
-      <img className={css(styles.logoImage)} src="../../assets/images/logo_white.png"/>
+      <img
+        className={css(styles.logoImage)}
+        src="../assets/images/logo_white.png"
+      />
       <span className={css(styles.title)}>Estetico</span>
     </h1>
   </div>
+)
 
 export default Header
